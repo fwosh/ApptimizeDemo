8860073d41b240977db81c3e6ec81052a73eed1c
Karen Li <fwoshy@gmail.com>
2017-11-16 23:37:56 -0800

    first commit

---
+++ app/src/main/java/com/example/karenli/apptimizedemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.karenli.apptimizedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+//Add Apptimize import statements
+import com.apptimize.Apptimize;
+import com.apptimize.ApptimizeVar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declare three dynamic variables for use on Apptimize
+    private static ApptimizeVar<String> appText = ApptimizeVar.createString("Text", "Apptimize Me!");
+    private static ApptimizeVar<String> appColor = ApptimizeVar.createString("Background Color | magenta, green or blue", "magenta");
+    private static ApptimizeVar<Boolean> appButtonToggle = ApptimizeVar.createBoolean("Button Visibility", true);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Apptimize and include your app key
+        Apptimize.setup(this, "CVhHRhmUzvn6eh84Cnexxkf2NMeLXMg");
+        Apptimize.track("App loaded");
+
+        //Set the text to the String variable from Apptimize
+        TextView mainText = (TextView) findViewById(R.id.mainText);
+        mainText.setText(appText.value());
+
+        //Set the color depending on keywords from Apptimize
+        if(appColor.value() == "magenta"){
+            mainText.setBackgroundColor(Color.MAGENTA);
+        }
+        else if (appColor.value() == "green"){
+            mainText.setBackgroundColor(Color.GREEN);
+        }
+        else{
+            mainText.setBackgroundColor(Color.BLUE);
+        }
+
+        //Toggle button visibility depending on boolean from Apptimize
+        Button okButton = (Button) findViewById(R.id.okButton);
+        if (!appButtonToggle.value()){
+            okButton.setVisibility(View.GONE);
+        }
+
+    }
+}
